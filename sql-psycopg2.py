96edcdaedd160f85bad3ce28aa512fa823b86c00
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-05-15 17:02:14 +0000

    Add to sql-psycopg2.py for testing connection to chinook.

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,18 +7,35 @@ connection = psycopg2.connect(database="chinook")
 cursor = connection.cursor()
 
 
+#  cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
-cursor.execute('SELECT * FROM "Artist"')
+
+
+#cursor.execute('SELECT * FROM "Artist"')
+
+
+#  cursor.execute('SELECT "Name" FROM "Artist"')
+
+
+#  cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+
+#  cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
 
 results = cursor.fetchall()
 
 
-# results = cursor.fetchone()
+
+#  results = cursor.fetchone()
 
 
 connection.close()
 
 
-for results in results:
+for result in results:
     print(result)
 
